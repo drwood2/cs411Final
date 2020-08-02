5ef1bc7fcd1063827fe8069ef3d88e6d121aeb94
derek_wood <derek_wood@apple.com>
2020-08-02 12:12:46 -0700

    tables

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -3,12 +3,18 @@
 
 DROP TABLE IF EXISTS maker;
 DROP TABLE IF EXISTS req;
-DROP TABLE IF EXISTS FinalSched;
+DROP TABLE IF EXISTS FinalSchedule;
+DROP TABLE IF EXISTS Locations;
+DROP TABLE IF EXISTS Times;
 
 CREATE TABLE maker (
   id SERIAL PRIMARY KEY,
   username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+  password TEXT NOT NULL,
+  firstName TEXT NOT NULL,
+  lastName TEXT NOT NULL,
+  email TEXT NOT NULL,
+  orgId TEXT NOT NULL
 );
 
 CREATE TABLE req (
@@ -23,14 +29,28 @@ CREATE TABLE req (
   FOREIGN KEY (maker_id) REFERENCES maker (id)
 );
 
-CREATE TABLE FinalSched (
+CREATE TABLE FinalSchedule (
   id SERIAL PRIMARY KEY,
   maker_id INTEGER NOT NULL,
-  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   req_date TEXT NOT NULL,
-  req_time TEXT NOT NULL,
-  location TEXT NOT NULL,
-  priority INTEGER NOT NULL,
+  firstName TEXT NOT NULL,
+  lastName TEXT NOT NULL,
   capacity INTEGER NOT NULL,
+  location TEXT NOT NULL,
+  req_time TEXT NOT NULL,
   FOREIGN KEY (maker_id) REFERENCES maker (id)
+);
+
+CREATE TABLE Locations (
+  locationId SERIAL PRIMARY KEY,
+  locationName TEXT NOT NULL,
+  locationCapacity INTEGER NOT NULL
+);
+
+CREATE TABLE Times (
+  id SERIAL PRIMARY KEY,
+  locationId INTEGER NOT NULL,
+  locationName TEXT NOT NULL,
+  locationCapacity INTEGER NOT NULL,
+  FOREIGN KEY (locationId) REFERENCES Locations (locationId)
 );
