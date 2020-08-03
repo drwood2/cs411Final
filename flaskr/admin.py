58938278209c436a4e4b428848e69233a9ea17e0
derek_wood <derek_wood@apple.com>
2020-08-03 16:38:45 -0700

    module

--- flaskr/admin.py
+++ flaskr/admin.py
@@ -1,7 +1,7 @@
 from flask import (Blueprint, flash, g, redirect,
 render_template, request, session, url_for)
 
-from flask_admin import Admin
+#from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from werkzeug.exceptions import abort
 
